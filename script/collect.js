b2760724bfff2b676f15751b50617803d0271fc6
Łukasz Szewczak <lukaszewczak@gmail.com>
2018-04-09 06:14:53 +0200

    move error handling to collect function

--- script/collect.js
+++ script/collect.js
@@ -10,37 +10,36 @@ collect()
 
 async function collect () {
   for (const version of nodeVersions) {
-    await getDocsForNodeVersion(version)
+    await getDocsForNodeVersion(version).catch(err => {
+      console.error(`problem fetching version: ${version}`)
+      console.error(err)
+    })
   }
 }
 
 async function getDocsForNodeVersion (version) {
-  try {
-    const docDir = path.join(__dirname, `../content/${version}/en-US/doc`)
-    const tempDir = path.join(__dirname, `../temp/${version}`)
+  const docDir = path.join(__dirname, `../content/${version}/en-US/doc`)
+  const tempDir = path.join(__dirname, `../temp/${version}`)
 
-    // exit early if docs for this version have already been downloaded
-    if (fs.existsSync(docDir)) {
-      console.log(`docs for ${version} have already been collected; skipping`)
-      return
-    }
-
-    // download repo bundle and extract to a temporary directory
-    const tarballUrl = `https://github.com/nodejs/node/archive/${version}.tar.gz`
-    console.log('downloading', tarballUrl)
-    await download(tarballUrl, tempDir, {extract: true})
-
-    // move docs from temp dir to this repo
-    const tempDocDir = path.join(tempDir, `node-${version.replace('v', '')}`, 'doc')
-    await fs.move(tempDocDir, docDir)
-
-    // keep .md files and remove all others
-    walk(docDir, {directories: false})
-      .filter(file => path.extname(file.relativePath.toLowerCase()) !== '.md')
-      .forEach(file => fs.unlinkSync(path.join(docDir, file.relativePath)))
-
-    fs.remove(tempDir)
-  } catch (ex) {
-    console.error(`error during getting docs for ${version}: ${ex}`)
+  // exit early if docs for this version have already been downloaded
+  if (fs.existsSync(docDir)) {
+    console.log(`docs for ${version} have already been collected; skipping`)
+    return
   }
+
+  // download repo bundle and extract to a temporary directory
+  const tarballUrl = `https://github.com/nodejs/node/archive/${version}.tar.gz`
+  console.log('downloading', tarballUrl)
+  await download(tarballUrl, tempDir, {extract: true})
+
+  // move docs from temp dir to this repo
+  const tempDocDir = path.join(tempDir, `node-${version.replace('v', '')}`, 'doc')
+  await fs.move(tempDocDir, docDir)
+
+  // keep .md files and remove all others
+  walk(docDir, {directories: false})
+    .filter(file => path.extname(file.relativePath.toLowerCase()) !== '.md')
+    .forEach(file => fs.unlinkSync(path.join(docDir, file.relativePath)))
+
+  fs.remove(tempDir)
 }
