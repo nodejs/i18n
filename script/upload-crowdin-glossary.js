d2d9b20287053741d418a2dfbfc550ea8809bf7b
Łukasz Szewczak <lukasz.szewczak@riscosoftware.pl>
2018-05-15 13:59:42 +0200

    add script to build and upload crowdin glossary

---
+++ script/upload-crowdin-glossary.js
@@ -0,0 +1,23 @@
+require('dotenv-safe').load()
+
+const globals = require('globals')
+const nodeCoreModules = require('builtins')('10.0.0')
+const glossary = require('crowdin-glossary')({project: 'nodejs'})
+
+main()
+
+async function main () {
+  // JavaScript builtins like Array, Map, String, etc
+  Object.keys(globals.builtin).forEach(term => {
+    if (Object.keys(glossary.entries).includes(term)) return
+    glossary.add(term, 'This is a JavaScript builtin and should usually not be translated')
+  })
+
+  // Node.js core modules
+  nodeCoreModules.forEach(coreModule => {
+    if (Object.keys(glossary.entries).includes(coreModule)) return
+    glossary.add(coreModule, 'This is a Node.js core module and should usually not be translated')
+  })
+
+  glossary.upload()
+}
