beeeab269f1d31bd70fdcf14909e913c24b734d2
Nick Schonning <nschonni@gmail.com>
2020-04-14 20:21:15 -0400

    feat: checking for tranlated files not in en-US

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ const path = require('path')
 const walk = require('walk-sync').entries
 const { supportedVersions } = require('./package.json')
 const contentDir = path.join(__dirname, 'content')
+const dircompare = require('dir-compare');
 
 test('includes source English content for all major versions', () => {
   const fetchedMajors = fs.readdirSync(contentDir).sort()
@@ -30,6 +31,41 @@ test('includes only markdown files, ignoring images and other files', () => {
       const files = walk(docsDir, { directories: false })
       expect(files.length).toBeGreaterThan(0)
       expect(files.every(file => file.relativePath.endsWith('.md')))
+    })
+  })
+})
+
+describe('no translated files that aren\'t in en-US', () => {
+
+  const options = { compareSize: false, noDiffSet: false };
+
+  const fetchedMajors = fs.readdirSync(contentDir).sort()
+
+  fetchedMajors.forEach(major => {
+    const languages = fs.readdirSync(path.join(contentDir, major))
+    const path1 = path.join(contentDir, major, 'en-US');
+
+    languages.forEach(lang => {
+      if (lang === 'en-US') return
+      test(`${major} ${lang}`, () => {
+        const path2 = path.join(contentDir, major, lang);
+
+        const result = dircompare.compareSync(path1, path2, options)
+
+        if (result.differences > 0 )
+        {
+          result.diffSet.forEach(diff => {
+            // only look for files only existing in translated content
+            if (diff.state === "right") {
+              expect(path.join(diff.relativePath, diff.name2)).toBeUndefined()
+            }
+          })
+        }
+        else
+        {
+          expect(result.differences).toEqual(0)
+        }
+      })
     })
   })
 })
